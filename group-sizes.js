edabc59f9b4ed9e8c788b2335672267f4488bed1
Sarah Lopez <slopez323@gmail.com>
2022-02-07 14:58:28 -0500

    update group-sizes text

--- group-sizes.js
+++ group-sizes.js
@@ -3,7 +3,7 @@ const prompt = require('prompt-sync')();
 let classSize = Number(prompt("How many students are in the class? "));
 
 if (classSize % 3 === 0) {
-    console.log("There are " + (classSize / 3) + " group/s of 3.")
+    console.log("There are " + (classSize / 3) + " group/s of 3 and 0 group/s of 2.")
 } else if (classSize > 3 && classSize % 3 === 1) {
     console.log("There are " + ((classSize-4) / 3) + " group/s of 3 and 2 group/s of 2.")
 } else if (classSize > 3 && classSize % 3 === 2) {
